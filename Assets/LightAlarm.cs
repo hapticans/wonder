1ff3bbd34cc60533a467528e5b2e5522d57b0465
Jan Speckamp <speckij@gmail.com>
2018-06-25 20:01:59 +0200

    Added visual alarm

---
+++ Assets/LightAlarm.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightAlarm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        SetAlarm();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetAlarm()
+    {
+        StartCoroutine("redAlarm");
+    }
+
+    IEnumerator redAlarm()
+    {
+        while (true)
+        {
+            Light light = GetComponent<Light>();
+            for(int i=1; i<=8; i++) { 
+                light.intensity = 0.1f * i;
+                yield return new WaitForSeconds(0.1f);
+            }
+            for (int i = 8; i >= 1; i--)
+            {
+                light.intensity = 0.1f * i;
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
