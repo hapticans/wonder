d9f8bac5d8cc2b96928240f2175ab17374058f13
Tobias Kröger <tobkro@users.noreply.github.com>
2018-07-02 22:52:55 +0200

    Included Animated Knobs, and added one to the procedure

--- Assets/CustomKnob.cs
+++ Assets/CustomKnob.cs
@@ -8,43 +8,26 @@ public class CustomKnob : MonoBehaviour {
 	// EMS - related
 	public Ems_Handler ems_handler;
 	// Button Animation related
-	public float returnSpeed = 10.0f;
-	public float activationDistance = 0.5f;
 	private bool pressed = false;
 	private bool released = false;
-	private Vector3 startPosition;
-	private Quaternion startRot;
-
-
+	private float startRotation;
 
 	private Material mat;
-
 	private Color prevColor;
-
-
-	private bool istriggering;
-
-	public GameObject controller;
-
-  String basis = "OutputLED_";
 
     // Use this for initialization
     void Start () {
-		startPosition = transform.localPosition; // Reset position for Button Animation
 		mat = GetComponent<Renderer>().material;
     prevColor = mat.color;
-		startRot = transform.rotation;
-
+		startRotation = transform.eulerAngles.y;
     }
 
 	void Update () {
-
-		//handleEMS();
-		transform.rotation=Quaternion.Lerp (transform.rotation, startRot, 15.0f*Time.deltaTime);
-		//AnimatedKnobUpdate();
+		handleEMS();
+		AnimatedKnobUpdate();
 	}
 
-    public void checkKnob(String direction)		// direction must be A or B in the case of the standard knob
+    public void checkKnob(String direction)		// direction must be L for left or R for right in the case of the standard knob
     {
 		if (PersistentManager.Instance.markIfValidStep(name + "_" + direction))
         {
@@ -73,19 +56,19 @@ public class CustomKnob : MonoBehaviour {
         }
     }
 
-	IEnumerator waitOneSecond()
-	{
-		yield return new WaitForSeconds(1);
-	}
+		IEnumerator waitOneSecond()
+		{
+			yield return new WaitForSeconds(1);
+		}
 
-    IEnumerator resetColor()
-    {
-        yield return new WaitForSeconds(1);
-		GetComponent<Renderer>().material.color = prevColor;
-    }
+  	IEnumerator resetColor()
+  	{
+    	yield return new WaitForSeconds(1);
+			GetComponent<Renderer>().material.color = prevColor;
+  	}
 
-    void handleEMS(){
-    	if (PersistentManager.Instance.isStepValid(name)){
+    void handleEMS(){  // TODO: Include feedback for starting to flip into the wrong direction
+    	if (PersistentManager.Instance.isStepValid(name + "_L") || PersistentManager.Instance.isStepValid(name + "_R")){
 				ems_handler.CheckEMS_rightButton(transform.position);
 			}
 			else{
@@ -95,40 +78,27 @@ public class CustomKnob : MonoBehaviour {
 
 		void AnimatedKnobUpdate(){
 			released = false;
-			float distance;
+			float difference;
+			difference = transform.eulerAngles.y - startRotation;
+			//Debug.Log(transform.eulerAngles.y);
 
-			Vector3 localPos = transform.localPosition;
-			localPos.x = startPosition.x;
-			localPos.z = startPosition.z;
-			//localPos.y = Mathf.Clamp(localPos.y, -0.4f, 0f);
-
-			transform.localPosition = localPos;
-
-			// Move back to startPosition if not obstructed
-			transform.localPosition = Vector3.Lerp(transform.localPosition, startPosition, Time.deltaTime * returnSpeed);
-
-			// trigger distance calculation
-			Vector3 xyzdistance = transform.localPosition - startPosition;
-			distance = Math.Abs(xyzdistance.y);
-
-
-			float pressComplete = Mathf.Clamp(1 / activationDistance * distance, 0f, 1f);
-
-			//Activate when pressed
-			if (pressComplete >= 0.025f && !pressed && !released)
-			{
-					// TODO: Insert Triggered events here
-					pressed = true;
-					Debug.Log("pressed " + localPos.y);
-
+			if(transform.eulerAngles.y > 30 && transform.eulerAngles.y < 50 && !pressed && !released){
+				//checkKnob("R");
+				pressed = true;
+				checkKnob("R");
+				Debug.Log("right " + name + "_R");
 			}
-			//Deactivate when released
-			else if (pressComplete <= 0.01f && pressed)
-			{
-					pressed = false;
-					released = true;
-					Debug.Log("released " + localPos.y);
+			else if(transform.eulerAngles.y < 230 && transform.eulerAngles.y > 210 && !pressed && !released){
+				pressed = true;
+				Debug.Log("left " + name + "_L");
+				checkKnob("L");
 			}
+			else if(transform.eulerAngles.y > 310 && transform.eulerAngles.y < 335 && pressed){
+				pressed = false;
+				released = true;
+				Debug.Log("released");
+			}
+
 		}
 
     //false entspricht alles geschafft
@@ -167,9 +137,6 @@ public class CustomKnob : MonoBehaviour {
     public void feedbackForFailure(){
 		Application.Quit();
         mat.color = Color.black;
-        // SceneManager.UnloadSceneAsync("Scene");
-        //Debug.Log(basis + (PersistentManager.Instance.getStrikes() + 1));
-        // Destroy(GameObject.Find("[CameraRig]"));
 
         StartCoroutine(resetColor());
         Debug.Log("Failed!");
